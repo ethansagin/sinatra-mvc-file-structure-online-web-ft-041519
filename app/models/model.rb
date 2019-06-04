7f97cdf683da6778ba1541a5275e0f0657c0db72
Ethan Sagin <ethansagin@gmail.com>
2019-06-04 14:18:46 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,12 @@
-class Model
-
+class Dog
+  attr_accessor :name, :breed, :age
+  
+  @@all = []
+  
+  def initialize(name, age, breed)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+  
 end

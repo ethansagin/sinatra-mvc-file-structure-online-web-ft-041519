09e93d89a3b85b2c2bd264d7c44c53f3fca3df5d
Ethan Sagin <ethansagin@gmail.com>
2019-06-04 14:23:33 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -3,18 +3,10 @@ class Dog
   
   @@all = []
   
-  def initialize(:name, :age, :breed)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @@all << self
-  end
-  
-  def self.create(name, breed, age)
-    self.new(name, breed, age).save
-  end
-  
-  def save
     @@all << self
   end
   
